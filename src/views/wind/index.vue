da5dcdc6fe8886e7b98a40204189499a468ae1c6
zhouyq <501962359@qq.com>
2019-10-30 19:01:57 +0800

    定位首都机场

--- src/views/wind/index.vue
+++ src/views/wind/index.vue
@@ -10,6 +10,7 @@ import  widgets from'cesium/Widgets/widgets.css';
 export default {
    name: 'cesiumContainer',
    mounted(){
+      // Cesium.Camera.DEFAULT_VIEW_RECTANGLE = Cesium.Rectangle.fromDegrees(71.39628233299722, 17.90751494736041, 137.14821935043528, 49.06704603525708)
       this.viewer = new Cesium.Viewer('cesiumContainer', {
       animation: false,
       shouldAnimate: true,
@@ -24,7 +25,24 @@ export default {
         enablePickFeatures: false
       })
     })
+    let camera = this.viewer.camera;
+    camera.setView({
+        destination : Cesium.Cartesian3.fromDegrees(116.576534748692, 40.0780145185529, 500),
+        orientation: {
+            heading : 359.668148999818,
+            pitch : -88.8329210486802,
+            roll : 0.0
+        }
+    });
 
+    this.viewer.camera.flyTo({
+      destination: Cesium.Cartesian3.fromDegrees(116.576534748692, 40.0780145185529, 21961.9883961571),
+      orientation: {
+        heading: Cesium.Math.toRadians(359.668148999818),
+        pitch: Cesium.Math.toRadians(-88.8329210486802),
+        roll: Cesium.Math.toRadians(0)
+      }
+    })
     this.viewer._cesiumWidget._creditContainer.style.display = "none";
     Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlZjY5ODg0MS1lZTMxLTRmMGMtOTRhYi00N2M2YjQ3ZDMzNjgiLCJpZCI6NDgwLCJpYXQiOjE1MjUyNTE1Nzh9.5Mi3ijReKCRQ_Shupv2w-wl2eJBRLOOW3Bmeq0IL5Y4'
   }
